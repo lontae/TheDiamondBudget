a722cc03a3c88c0f69cf7e8b1acce9b6a8255f66
Rod <hrod3112002@yahoo.com>
2022-03-24 20:27:40 -0400

    html changes

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const expenseE1 = document.querySelector("#expense");
 const incomeList = document.querySelector("#income .list");
 const expenseList = document.querySelector("#expense .list");
 
-const chartE1 = document.querySelector("Chart");  //use an Image, it does not have to work 
-
+const chartE1 = document.querySelector("Chart"); //use an Image, it does not have to work
 
 const incomeAmount = document.getElementById("income-amount-input");
 const addIncome = document.querySelector(" .add-income");
@@ -18,79 +17,77 @@ const expenseTitle = document.getElementById("expense-title-input");
 const expenseAmount = document.getElementById("expense-amount-input");
 // buttons - may need to add to html
 
-
 let ENTRY_LIST = [];
-let balance = 0, income = 0, outcome = 0;
+let balance = 0,
+  income = 0,
+  outcome = 0;
 
-const DELETE = "delete", EDIT = "edit";
+const DELETE = "delete",
+  EDIT = "edit";
 // variables??
 
-
-
 addIncome.addEventListener("click", function () {
-    if (!incomeTitle.value || incomeAmount.value) return;
-    let income = {
-        type: "income",
-        title: incomeTitle.value,
-        amount: parseFloat(incomeAmount.value),
-    }
-    ENTRY_LIST.push(income);
+  if (!incomeTitle.value || incomeAmount.value) return;
+  let income = {
+    type: "income",
+    title: incomeTitle.value,
+    amount: parseFloat(incomeAmount.value),
+  };
+  ENTRY_LIST.push(income);
 
-    updateUI();
-    clearInput([incomeTitle, incomeAmount]);
+  updateUI();
+  clearInput([incomeTitle, incomeAmount]);
 });
 
 addExpense.addEventListener("click", function () {
-    if (!expenseTitle.value || expenseAmount.value) return;
-    // if nothing is entered exit the entry
-    let expense = {
-        type: "expense",
-        title: expenseTitle.value,
-        amount: parseFloat(expenseAmount.value),
-    }
-    ENTRY_LIST.push(expense);
+  if (!expenseTitle.value || expenseAmount.value) return;
+  // if nothing is entered exit the entry
+  let expense = {
+    type: "expense",
+    title: expenseTitle.value,
+    amount: parseFloat(expenseAmount.value),
+  };
+  ENTRY_LIST.push(expense);
 
-    updateUI();
-    clearInput([expenseTitle, expenseAmount]);
+  updateUI();
+  clearInput([expenseTitle, expenseAmount]);
 });
 
 function updateUI() {
-    income = calculateTotal("income", ENTRY_LIST);
-    outcome = calculateTotal("outcome", ENTRY_LIST);
-    balance = calculateTotal("balance", ENTRY_LIST);
+  income = calculateTotal("income", ENTRY_LIST);
+  outcome = calculateTotal("outcome", ENTRY_LIST);
+  balance = calculateTotal("balance", ENTRY_LIST);
 
-    incomeTotalE1.innerHTML = income
-    outcomeTotalE1.innerHTML = outcome
-    //this will calculate and upate the visual
+  incomeTotalE1.innerHTML = income;
+  outcomeTotalE1.innerHTML = outcome;
+  //this will calculate and upate the visual
 }
 
 function calculateTotal(type, list) {
-    let sum = 0;
+  let sum = 0;
 
-    list.forEach(entry => {
-        if (entry.type == type) {
-            sum += entry.amount;
-        }
-    })
+  list.forEach((entry) => {
+    if (entry.type == type) {
+      sum += entry.amount;
+    }
+  });
 
-    return sum;
+  return sum;
 }
 function calculateBalance(income, outcome) {
-    return income - outcome;
+  return income - outcome;
 }
-
-
-
-
 
 editIncome.addEventListener("click", function (idx) {
-    if (!incomeTitle.value || incomeAmount.value) return;
+  if (!incomeTitle.value || incomeAmount.value) return;
 
-    ENTRY_LIST[idx].amount = calculateBalance(incomeAmount.value, outcome)
-    updateUI();
-    clearInput([incomeTitle, incomeAmount]);
-})
-const clickMe =() => {
-    console.log("you click me")
-}
-console.log("bills")
+  ENTRY_LIST[idx].amount = calculateBalance(incomeAmount.value, outcome);
+  updateUI();
+  clearInput([incomeTitle, incomeAmount]);
+});
+const clickMe = () => {
+  function clickMe()
+  console.log("bills");
+  window.location = "bills.html";
+};
+
