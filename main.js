5680b068e6ea5cddef2cf9bebaff724bc0a061a6
Rod <hrod3112002@yahoo.com>
2022-03-26 14:36:48 -0400

    html update

--- main.js
+++ main.js
@@ -99,8 +99,15 @@ const clickMe = (e) => {
   console.log("bills",e.target.value);
   setTimeout(() => {
     window.location.href = e.target.value
-  },  5000);
+  },  4300);
 };
 const goBackHome = (e) => {
  window.location.href = 'index.html'
  };
+ function add(){
+  var n1 = parseInt(document.getElementById('n1').value);
+  var n2 = parseInt(document.getElementById('n2').value);
+  var n3 = parseInt(document.getElementById('n3').value);
+  var sum = n1 + n2 + n3;
+  document.getElementById("results").innerHTML=sum;
+};
